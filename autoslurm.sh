3a0f3d1f4b2d697256d515e37bc4584506469907
George Flanagin <gflanagi@richmond.edu>
2022-07-27 14:33:50 -0400

    Added a comment to the shell script.

--- autoslurm.sh
+++ autoslurm.sh
@@ -1,8 +1,11 @@
 autoslurm()
 {
     export PYTHONPATH=/usr/local/sw/hpclib
+    # Instead of using $OLDPWD as the location we came from to run
+    # autoslurm, let's set an environment variable so that we can
+    # know that this shell function launched autoslurm.
     export AUTOSLURM_DEFAULT_DIR="$PWD"
     command pushd /usr/local/sw/autoslurm.dev >/dev/null
-    python autoslurm.py --dryrun $@
+    python autoslurm.py $@
     command popd >/dev/null
 }
